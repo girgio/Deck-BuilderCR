831b302c98129e8b50ba34d7060f23ec5d4d3fb3
Giovanni De Caro <nannidecaro@gmail.com>
2026-01-27 18:48:30 +0100

    Test per fitness
    Fatti alcuni test per avere fitness di specifici deck per la presentazione

--- DeckBuilderCR/Dati/test.py
+++ DeckBuilderCR/Dati/test.py
@@ -4,18 +4,18 @@ from Mazzo import Mazzo
 from DatabaseCarte import DatabaseCarte
 
 db = DatabaseCarte()
-carte1 = [db.get_carta("Cavaliere"),db.get_carta("Capanna goblin"),db.get_carta("Razzo"),db.get_carta("Scheletri"),db.get_carta("Minatore"),
-         db.get_carta("Megasgherro"),db.get_carta("Gigante"),db.get_carta("Arciere magico")]
-carte2 = [db.get_carta("Golem"),db.get_carta("Mastino lavico"),db.get_carta("Pekka"),db.get_carta("Reclute royale"),db.get_carta("Scintilla"),
-         db.get_carta("Gigante elletrico"),db.get_carta("Gran cavaliere"),db.get_carta("Imperatrice degli spiriti")]
-carte3 = [db.get_carta("Fulmine"),db.get_carta("Mastino lavico"),db.get_carta("Pekka"),db.get_carta("Reclute royale"),db.get_carta("Scintilla"),
-         db.get_carta("Gigante elletrico"),db.get_carta("Gigante scheletro"),db.get_carta("Imperatrice degli spiriti")]
+carte1 = [db.get_carta("Cavaliere"),db.get_carta("Principe"),db.get_carta("Stregone elletrico"),db.get_carta("Spirito della cura"),db.get_carta("Golem"),
+         db.get_carta("Cannone"),db.get_carta("Gigante"),db.get_carta("Scarica")]
+carte2 = [db.get_carta("Golem del ghiaccio"),db.get_carta("Berserker"),db.get_carta("Cannone"),db.get_carta("Tesla"),db.get_carta("Arco x"),
+         db.get_carta("Gigante elletrico"),db.get_carta("Arciere magico"),db.get_carta("Barile goblin")]
+carte3 = [db.get_carta("Strega"),db.get_carta("Cavaliere"),db.get_carta("Pekka"),db.get_carta("Frecce"),db.get_carta("Lapide"),
+         db.get_carta("Cacciatore"),db.get_carta("Valchiria"),db.get_carta("Tronco")]
 carte4 = [db.get_carta("Scheletri"),db.get_carta("Spirito del ghiaccio"),db.get_carta("Spirito del fuoco"),db.get_carta("Spirito elletrico"),db.get_carta("Goblin"),
          db.get_carta("Spirito della cura"),db.get_carta("Scarica"),db.get_carta("Guardie")]
 carte5 = [db.get_carta("Domatrice di arieti"),db.get_carta("Stregone di ghiaccio"),db.get_carta("Gigante elletrico"),db.get_carta("Mascalzoni"),db.get_carta("Palla di neve gigante"),
          db.get_carta("Torre infernale"),db.get_carta("Drago elletrico"),db.get_carta("Strega madre")]
-carte6 = [db.get_carta("Gigante royale"),db.get_carta("Barile goblin"),db.get_carta("Sgherri"),db.get_carta("Strega"),db.get_carta("Stregone"),
-         db.get_carta("Scarica"),db.get_carta("Tesla"),db.get_carta("Orda di scheletri")]
+carte6 = [db.get_carta("Tronco"),db.get_carta("Macchina volante"),db.get_carta("Sgherri"),db.get_carta("Spirito del ghiaccio"),db.get_carta("Cacciatore"),
+         db.get_carta("Strega"),db.get_carta("Domatore di cinghiali"),db.get_carta("Gran cavaliere")]
 deck = Mazzo(carte1)
 deck2 = Mazzo(carte2)
 deck3 = Mazzo(carte3)
--- DeckBuilderCR/test.py
+++ DeckBuilderCR/test.py
@@ -8,46 +8,46 @@ from Algoritmi.HillClimbing import hill_climbing, costruisci_mazzo_random_valido
 
 def main():
     # Seed forte per evitare qualsiasi ripetibilità accidentale
-    random.seed(time.time_ns() ^ os.getpid())
+        random.seed(time.time_ns() ^ os.getpid())
 
-    db = DatabaseCarte("Dati/database_carte.json")
+        db = DatabaseCarte("Dati/database_carte.json")
 
-    raw = input(
-        "Inserisci da 1 a 4 carte fisse (nomi separati da virgola), "
-        "oppure premi Invio per nessuna: "
-    ).strip()
+        raw = input(
+            "Inserisci da 1 a 4 carte fisse (nomi separati da virgola), "
+            "oppure premi Invio per nessuna: "
+        ).strip()
 
-    fisse = set()
-    if raw:
-        fisse = {x.strip() for x in raw.split(",") if x.strip()}
-        if not (1 <= len(fisse) <= 4):
-            raise ValueError("Devi inserire da 1 a 4 carte fisse.")
+        fisse = set()
+        if raw:
+            fisse = {x.strip() for x in raw.split(",") if x.strip()}
+            if not (1 <= len(fisse) <= 4):
+                raise ValueError("Devi inserire da 1 a 4 carte fisse.")
 
-    #MAZZO INIZIALE CASUALE
-    mazzo_iniziale = costruisci_mazzo_random_valido(db, fisse)
+        #MAZZO INIZIALE CASUALE
+        mazzo_iniziale = costruisci_mazzo_random_valido(db, fisse)
 
-    print("\n==============================")
-    print("MAZZO INIZIALE")
-    print("==============================")
-    print([c.nome for c in mazzo_iniziale.carte])
+        print("\n==============================")
+        print("MAZZO INIZIALE")
+        print("==============================")
+        print([c.nome for c in mazzo_iniziale.carte])
 
-    best, fit = hill_climbing(
-        mazzo_iniziale=mazzo_iniziale,
-        db=db,
-        fisse=fisse,
-        max_iter=200,
-        neighbors_per_iter=60,
-        max_stalli=30,
-        usa_restart=True,
-        seed=None,
-        stampa_finale=False
-    )
+        best, fit = hill_climbing(
+            mazzo_iniziale=mazzo_iniziale,
+            db=db,
+            fisse=fisse,
+            max_iter=200,
+            neighbors_per_iter=60,
+            max_stalli=30,
+            usa_restart=True,
+            seed=None,
+            stampa_finale=False
+        )
 
-    print("\n==============================")
-    print("MAZZO FINALE")
-    print("==============================")
-    print([c.nome for c in best.carte])
-    print("FITNESS:", fit)
+        print("\n==============================")
+        print("MAZZO FINALE")
+        print("==============================")
+        print([c.nome for c in best.carte])
+        print("FITNESS:", fit)
 
 
 if __name__ == "__main__":
